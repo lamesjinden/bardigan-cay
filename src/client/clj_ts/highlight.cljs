b1271cd0ade858546117434272c24a4ffb27a6e0
james <7298563+lamesjinden@users.noreply.github.com>
2023-11-20 22:40:20 -0500

    Load highlight.js module from cljs over script tag.

--- src/client/clj_ts/highlight.cljs
+++ src/client/clj_ts/highlight.cljs
@@ -1,4 +1,13 @@
-(ns clj-ts.highlight)
+(ns clj-ts.highlight
+  (:require ["highlight.js/lib/core" :default hljs]
+            ["highlight.js/lib/languages/bash" :as bash]
+            ["highlight.js/lib/languages/clojure" :as clojure]
+            ["highlight.js/lib/languages/markdown" :as markdown]))
+
+(.registerLanguage hljs "markdown" markdown)
+(.registerLanguage hljs "bash" bash)
+(.registerLanguage hljs "clojure" clojure)
+
 
 (defn highlight-all []
-  (.highlightAll js/hljs))
+  (.highlightAll hljs))
