af1d1df6b99550072d2ba327f5097ee1a0a57247
james <7298563+lamesjinden@users.noreply.github.com>
2025-08-11 21:12:18 -0400

    Add 'csharp' language to highlight ns

--- src/client/clj_ts/highlight.cljs
+++ src/client/clj_ts/highlight.cljs
@@ -2,11 +2,13 @@
   (:require ["highlight.js/lib/core" :default hljs]
             ["highlight.js/lib/languages/bash" :as bash]
             ["highlight.js/lib/languages/clojure" :as clojure]
+            ["highlight.js/lib/languages/csharp" :as csharp]
             ["highlight.js/lib/languages/markdown" :as markdown]))
 
 (.registerLanguage hljs "markdown" markdown)
 (.registerLanguage hljs "bash" bash)
 (.registerLanguage hljs "clojure" clojure)
+(.registerLanguage hljs "csharp" csharp)
 
 (defn highlight-all []
   (.highlightAll hljs))
