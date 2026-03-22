2605d9efcac4057ffde097e5d60f8809b3648056
james linden <7298563+lamesjinden@users.noreply.github.com>
2026-03-22 12:23:50 -0400

    Update uberjar build

--- build/build.clj
+++ build/build.clj
@@ -1,20 +1,21 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]
-            [inline])
-  (:import [java.time LocalDateTime]
-           [java.time.format DateTimeFormatter]))
+  (:require [babashka.process :as p]
+            [clojure.string :as str]
+            [clojure.tools.build.api :as b]
+            [inline]))
 
-(def artifact-name "clj-ts")
-(def version "0.1.0-SNAPSHOT")
+(def artifact-name "bardigan-cay")
+(def version "1.0.0")
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"
                             :aliases [:server]}))
-(def uber-file
-  (format "target/%s-%s-standalone.%s.jar"
-          artifact-name
-          version
-          (.format (LocalDateTime/now)
-                   (DateTimeFormatter/ofPattern "yyyy-MM-dd-HH-mm-ss"))))
+(def uber-file (format "target/%s-%s.jar" artifact-name version))
+
+(defn git-short-hash []
+  (-> (p/process ["git" "rev-parse" "--short" "HEAD"] {:out :string})
+      deref
+      :out
+      str/trim))
 
 (defn uber [_]
   (b/copy-dir {:src-dirs   ["src" "resources"]
@@ -26,4 +27,8 @@
   (b/uber {:class-dir class-dir
            :uber-file uber-file
            :basis     basis
-           :main      'clj-ts.app}))
+           :main      'clj-ts.app
+           :manifest  {"Implementation-Title"   artifact-name
+                       "Implementation-Version" version
+                       "Git-Commit"             (git-short-hash)
+                       "Build-Timestamp"        (str (java.time.Instant/now))}}))
