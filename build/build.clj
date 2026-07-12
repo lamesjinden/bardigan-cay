c7de96cff37dc910acb7a9e95f3d89518d2ac700
james linden <7298563+lamesjinden@users.noreply.github.com>
2026-07-11 22:13:07 -0400

    Update build version (1.0.2)

--- build/build.clj
+++ build/build.clj
@@ -5,7 +5,7 @@
             [inline]))
 
 (def artifact-name "bardigan-cay")
-(def version "1.0.1")
+(def version "1.0.2")
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"
                             :aliases [:server]}))
