1d2b9091d94425dc725f8a255693275a9b0b1663
james linden <7298563+lamesjinden@users.noreply.github.com>
2026-08-06 21:35:26 -0400

    Update build artifact versioning scheme
    * add suffix to include BUILD_NUMBER env var and git SHA, when present
    * otherwise, add "-SNAPSHOT" suffix

--- build/build.clj
+++ build/build.clj
@@ -5,17 +5,25 @@
             [inline]))
 
 (def artifact-name "bardigan-cay")
-(def version "1.0.2")
-(def class-dir "target/classes")
-(def basis (b/create-basis {:project "deps.edn"
-                            :aliases [:server]}))
-(def uber-file (format "target/%s-%s.jar" artifact-name version))
+(def base-version "1.0.2")
 
 (defn git-short-hash []
   (-> (p/process ["git" "rev-parse" "--short" "HEAD"] {:out :string})
       deref
       :out
       str/trim))
+
+;; git-describe-style labeling: CI builds (Jenkins exports BUILD_NUMBER) are
+;; "<base>-<build>-g<sha>"; interactive builds are "<base>-SNAPSHOT".
+(def version
+  (if-let [build-number (System/getenv "BUILD_NUMBER")]
+    (format "%s-%s-g%s" base-version build-number (git-short-hash))
+    (str base-version "-SNAPSHOT")))
+
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"
+                            :aliases [:server]}))
+(def uber-file (format "target/%s-%s.jar" artifact-name version))
 
 (defn uber [_]
   (b/copy-dir {:src-dirs   ["src" "resources"]
